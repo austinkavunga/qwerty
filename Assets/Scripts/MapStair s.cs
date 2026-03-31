b9a36c3f3d95baecb950f878c30f00bce089ab42
austinkavunga <austinkavunga@gmail.com>
2026-03-31 18:34:01 +0100

    changing map stairs script
    fixed item errors.

--- Assets/Scripts/MapStair s.cs
+++ Assets/Scripts/MapStair s.cs
@@ -2,78 +2,36 @@ using UnityEngine;
 
 public class MapStairs : MonoBehaviour
 {
-    public GameObject GroundFloor;
-    public GameObject TopFloor;
-    public GameObject GroundFloorStairs;
-    public GameObject TopFloorStairs;
-    public GameObject BasementStairs;
-    public GameObject BasementFloor;
-    public GameObject BasementLadder;
+    public GameObject groundFloor;
+    public GameObject firstFloor;
+    public GameObject basement;
     private Transform playerTransform;
 
     void Start()
     {
-        GroundFloor.SetActive(true);
-        GroundFloorStairs.SetActive(true);
-
-        TopFloor.SetActive(false);
-        TopFloorStairs.SetActive(false);
-
-        BasementFloor.SetActive(false);
-        BasementStairs.SetActive(true);
-        BasementLadder.SetActive(false);
-
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
-
-    void Update()
-    {
-        var ahhh = GroundFloorStairs.GetComponent<EdgeCollider2D>();
+        basement.SetActive(false);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("GroundFloorStairs"))
         {
-            GroundFloor.SetActive(false);
-            GroundFloorStairs.SetActive(false);
-            BasementStairs.SetActive(false);
-            TopFloor.SetActive(true);
-            TopFloorStairs.SetActive(true);
-            playerTransform.position = new Vector3(9, -12, playerTransform.position.z);
+            playerTransform.position = new Vector3(117, -12, playerTransform.position.z);
         }
         if (collision.gameObject.CompareTag("FirstFloorStairs"))
         {
-            TopFloor.SetActive(false);
-            TopFloorStairs.SetActive(false);
-            GroundFloor.SetActive(true);
-            GroundFloorStairs.SetActive(true);
-            BasementStairs.SetActive(true);
-            BasementLadder.SetActive(false);
             playerTransform.position = new Vector3(9, -7, playerTransform.position.z);
         }
         if(collision.gameObject.CompareTag("BasementStairs"))
         {
-            BasementFloor.SetActive(true);
-            BasementStairs.SetActive(false);
-            BasementLadder.SetActive(true);
-            GroundFloor.SetActive(false);
-            GroundFloorStairs.SetActive(false);
-            TopFloor.SetActive(false);
-            TopFloorStairs.SetActive(false);
-            playerTransform.position = new Vector3(19, -6, playerTransform.position.z);
+            basement.SetActive(true);
+            playerTransform.position = new Vector3(19, 42, playerTransform.position.z);
         }
         if(collision.gameObject.CompareTag("BasementLadder"))
         {
-            BasementFloor.SetActive(false);
-            BasementLadder.SetActive(false);
-            BasementStairs.SetActive(true);
-            GroundFloor.SetActive(true);
-            GroundFloorStairs.SetActive(true);
-            BasementStairs.SetActive(true);
-            TopFloor.SetActive(false);
-            TopFloorStairs.SetActive(false);
             playerTransform.position = new Vector3(20, -11, playerTransform.position.z);
+            basement.SetActive(false);
         }
     }
 }
